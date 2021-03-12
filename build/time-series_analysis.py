2ab28e5db811c6cdf9fb8445d6bcdf7b0e447cfc
odissea15 <odysseas.polycarpou@gmail.com>
2021-03-12 16:38:07 +0200

    added author in .py

--- build/time-series_analysis.py
+++ build/time-series_analysis.py
@@ -1,3 +1,6 @@
+#author: Odysseas Polycarpou
+#guid: 2210049p
+
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -13,8 +16,8 @@ from scipy.stats import stats
 import matplotlib.backends.backend_pdf
 import math
 import random
-from matplotlib import pyplot as plt 
-import numpy as np  
+from matplotlib import pyplot as plt
+import numpy as np
 import random
 import math
 import numpy as np
@@ -44,7 +47,7 @@ sns.set()
 def file(fileinput):
     if not ".csv" in fileinput:
         fileinput = "data/" + fileinput + ".csv"
-    
+
     df = pd.read_csv(fileinput,skiprows=0)
 
     return df
@@ -54,16 +57,16 @@ def file(fileinput):
 
 
 def main():
-    
+
     user_input = str(input("Please enter the name of the .csv file you want to view: "))
 
     df = file(user_input)
-    
+
     df.head()
     #Rename the columns
     df.columns = ['date', 'value']
     df.head()
-    
+
     df.date = pd.to_datetime(df.date)
     df.set_index('date', inplace=True)
     df.head()
@@ -76,7 +79,7 @@ def main():
     plt.legend(loc=2, prop={'size': 20})
     plt.savefig('timeseries_analysis/time_series_data' + time.strftime("%Y-%m-%d %H%M%S") + '.png')
 
-    
+
     plt.figure()
     print("Smoothing")
     values = df[['value']]
@@ -87,7 +90,7 @@ def main():
     plt.legend(loc=2, prop={'size': 20})
     plt.savefig('timeseries_analysis/smoothed_data' + time.strftime("%Y-%m-%d %H%M%S") + '.png')
 
-    
+
     plt.figure()
     values.diff().plot(figsize = (20,10), linewidth = 5, fontsize = 20)
     plt.xlabel('Date', fontsize = 30)
@@ -96,12 +99,12 @@ def main():
     plt.legend(loc=2, prop={'size': 20})
     plt.savefig('timeseries_analysis/differencing_data' + time.strftime("%Y-%m-%d %H%M%S") + '.png')
 
-    
+
     plt.figure()
     values = df['value']
     pd.plotting.autocorrelation_plot(values)
     plt.savefig('timeseries_analysis/autocorrelation' + time.strftime("%Y-%m-%d %H%M%S") + '.png')
-    
+
     df.corr()
     return
 
@@ -114,7 +117,3 @@ if __name__ == "__main__":
 
 
 # In[ ]:
-
-
-
-
